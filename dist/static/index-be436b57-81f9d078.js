e6e82862a9e855e5e5d41b6929749b17b6470785
Cody Lambert <97857969+Romanguy13@users.noreply.github.com>
2023-05-09 00:32:37 -0700

    delete dist

--- dist/static/index-be436b57-81f9d078.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-56aa5cc3-074af160.js";import"./sanity-369b7ea1.js";import"./json-inspector-b665b15a.js";export{m as DocumentPaneProvider,a as default};
