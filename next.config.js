1f98a5cb76f14ada06a2c19eb1030e32e2944cc4
Cody Lambert <codylambertdev@gmail.com>
2023-05-09 00:20:50 -0700

    allow domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+    ],
+    domains: ['cdn.sanity.io'],
   },
-  reactStrictMode: true,
+
+
 }
 
 module.exports = nextConfig
