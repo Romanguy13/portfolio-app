fedd44648747075cdb9134ad373330a9c8df8b3f
Cody Lambert <codylambertdev@gmail.com>
2023-05-09 18:12:26 -0700

    made project page always one column

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,9 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center w-16 h-16 mb-4">
-        <h1 className="text-4xl font-bold text-gray-800">Loading...</h1>
+        <h1 className="text-4xl font-bold text-black dark:text-white">
+          Loading...
+        </h1>
       </div>
     </div>
   );
--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -51,7 +51,7 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
   return (
     <main>
       <div className="grow grid grid-cols-1 md:grid-cols-12">
-        <div className="md:col-span-10 flex flex-col justify-center md:border-r-2 border-b-2 p-4 p-8 md:p-10 lg:p-12 xl:p-16 border-black dark:border-white">
+        <div className="md:col-span-10 flex flex-col justify-center md:border-r-2 border-b-2 p-4 md:p-10 lg:p-12 xl:p-16 border-black dark:border-white">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
             {project.title}
           </h1>
@@ -65,52 +65,34 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
             Live Demo
           </h1>
-          <GrLinkNext className="text-4xl md:text-5xl lg:text-6xl font-bold group-hover:translate-x-4 transition-all duration-500 ease-in-out" />
+          <GrLinkNext className="text-4xl md:text-5xl lg:text-6xl font-bold group-hover:translate-x-4 transition-all duration-200 ease-in-out dark:text-white" />
         </a>
       </div>
       <a
         href={project.url}
         target="_blank"
         rel="noopener noreferrer"
-        className="md:hidden flex bg-accent dark:bg-secondary border-b-2 border-black dark:border-white justify-between items-center p-8 group hover:cursor-pointer"
+        className="md:hidden flex bg-accent dark:bg-secondary border-b-2 border-black dark:border-white justify-between items-center p-4 group hover:cursor-pointer"
       >
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold col-span-1 flex justify-center items-center">
           Live Demo
         </h1>
-        <GrLinkNext className="text-5xl md:text-6xl lg:text-4xl font-bold col-span-1 flex justify-center items-center dark:text-white group-hover:translate-x-4 transition-all duration-500 ease-in-out" />
+        <GrLinkNext
+          color="white"
+          className="text-4xl md:text-5xl lg:text-4xl font-bold col-span-1 flex justify-center items-center dark:text-white group-hover:translate-x-4 transition-all duration-500 ease-in-out"
+        />
       </a>
+      <Image
+        src={urlForImage(project.mainImage).url() || ""}
+        alt={project.title}
+        width={1600}
+        height={1400}
+        className="w-full overflow-hidden max-w-4xl mx-auto md:px-10 md:pt-10"
+        priority
+      />
 
-      <div className="flex flex-col lg:flex-row">
-        <div className="flex flex-col max-w-3xl lg:w-1/2 mx-auto md:p-8">
-          <Image
-            src={urlForImage(project.mainImage).url() || ""}
-            alt={project.title}
-            width={1600}
-            height={1400}
-            className="w-full overflow-hidden pb-4"
-            priority
-          />
-          <div className="flex flex-col px-4 md:p-0">
-            <h2 className="text-2xl font-bold">Resources</h2>
-            <div className="flex flex-col">
-              {project.github && (
-                <GrGithub className="text-4xl md:text-5xl lg:text-6xl font-bold col-span-1 flex justify-center items-center dark:text-white hover:cursor-pointer hover:text-gray-500 transition-all duration-200 ease-in-out mt-4" />
-              )}
-              <PortableText
-                content={project.resources}
-                serializers={serializers}
-                className="prose dark:prose-dark"
-              />
-            </div>
-          </div>
-        </div>
-        {/* <a
-          href={project.github}
-          className="flex max-w-3xl mx-auto px-4 md:px-8 lg:px-10 xl:px-12"
-        >
-          <GrGithub className="text-4xl md:text-5xl lg:text-6xl font-bold col-span-1 flex justify-center items-center dark:text-white" />
-        </a> */}
-        <div className="flex flex-col max-w-3xl mx-auto p-4 md:p-8 lg:w-1/2">
+      <div className="flex flex-col max-w-4xl mx-auto px-4 md:px-10 ">
+        <div className="flex flex-col pt-4 md:pt-10">
           <div className="flex flex-col">
             <h2 className="text-2xl font-bold">Description</h2>
             <div className="flex flex-col">
@@ -122,7 +104,26 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
             </div>
           </div>
         </div>
+        <div className="flex flex-col mx-auto py-4 md:py-10">
+          <h2 className="text-2xl font-bold">Resources</h2>
+          <div className="flex flex-col">
+            <PortableText
+              content={project.resources}
+              serializers={serializers}
+              className="prose dark:prose-dark"
+            />
+            {project.github && (
+              <GrGithub className="text-4xl md:text-5xl lg:text-6xl font-bold col-span-1 flex justify-center items-center dark:text-white hover:cursor-pointer hover:text-gray-500 transition-all duration-200 ease-in-out dark:hover:text-gray-400 mt-4 mb-8" />
+            )}
+          </div>
+        </div>
       </div>
+      {/* <a
+          href={project.github}
+          className="flex max-w-3xl mx-auto px-4 md:px-8 lg:px-10 xl:px-12"
+        >
+          <GrGithub className="text-4xl md:text-5xl lg:text-6xl font-bold col-span-1 flex justify-center items-center dark:text-white" />
+        </a> */}
     </main>
   );
 }
